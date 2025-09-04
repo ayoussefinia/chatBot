57a0bcba703c1373f7eb3b0b06bd169a1898e5b9
Alex <alex.youssefinia@gmail.com>
2025-09-04 01:33:54 -0500

    added capacitor and eas for mobile builds

---
+++ android/app/src/main/java/com/myapp/todo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myapp.todo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.myapp.todo",
+  appName: "TodoApp",
+  webDir: "out", // fallback, but won’t be used for SSR
+  server: {
+    url: "https://todo-29ni.onrender.com",
+    cleartext: true,
+  },
+};
+
+export default config;
