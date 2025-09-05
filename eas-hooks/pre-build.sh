afdd8283a2c240111982293dab2406994fcf156c
Alex <alex.youssefinia@gmail.com>
2025-09-05 18:55:16 -0500

    Add pre-build hook to force Java 17

--- eas-hooks/pre-build.sh
+++ eas-hooks/pre-build.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "=== HELLO FROM HOOK ==="
+echo "=== Forcing Java 17 in EAS build VM ==="
+
+if [ -d "/usr/lib/jvm/java-17-openjdk-amd64" ]; then
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+elif [ -d "/usr/lib/jvm/java-17-openjdk" ]; then
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+fi
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "JAVA_HOME=$JAVA_HOME"
+java -version
