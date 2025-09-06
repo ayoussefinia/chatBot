2637e1632292d480aef9db832fe192e47a2e16e0
Alex <alex.youssefinia@gmail.com>
2025-09-05 19:17:15 -0500

    test

--- eas-hooks/pre-build.sh
+++ eas-hooks/pre-build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "=== Forcing Java 17 in EAS build VM ==="
-
-if [ -d "/usr/lib/jvm/java-17-openjdk-amd64" ]; then
-  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-elif [ -d "/usr/lib/jvm/java-17-openjdk" ]; then
-  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+if [[ -d /usr/libexec/java_home ]]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+elif [[ -d "/usr/lib/jvm/java-17-openjdk-amd64" ]]; then
+  export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
+elif [[ -d "/usr/lib/jvm/java-17-openjdk" ]]; then
+  export JAVA_HOME="/usr/lib/jvm/java-17-openjdk"
 fi
-
-export PATH=$JAVA_HOME/bin:$PATH
-
 echo "JAVA_HOME=$JAVA_HOME"
+# persist the variable for subsequent build phases
+set-env JAVA_HOME "$JAVA_HOME"
 java -version
